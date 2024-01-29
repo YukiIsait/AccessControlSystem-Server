08d2a12e40263c49f3405b5a5ab42ea45a86eccd
YukiIsait <yukiisait@outlook.jp>
2024-01-29 22:41:37 +0800

    不缓存响应

--- src/main/kotlin/tech/youko/acms/controller/device/AccessibilityController.kt
+++ src/main/kotlin/tech/youko/acms/controller/device/AccessibilityController.kt
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 class AccessibilityController {
     @GetMapping
     fun accessibility(): Boolean {
-        return 2 / 0 > 0
+        return true
     }
 }
