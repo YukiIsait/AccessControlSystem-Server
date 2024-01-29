f86e1b28a9b051a237230c27edc6d46a7e4316d5
YukiIsait <yukiisait@outlook.jp>
2024-01-29 22:52:48 +0800

    添加设备访问接口

--- src/main/kotlin/tech/youko/acms/controller/device/AccessibilityController.kt
+++ src/main/kotlin/tech/youko/acms/controller/device/AccessibilityController.kt
@@ -3,14 +3,18 @@ package tech.youko.acms.controller.device
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import tech.youko.acms.entity.id.AccessInfoId
+import tech.youko.acms.service.IAccessInfoService
 
 @RestController
-@RequestMapping(value = ["/device/accessibility"])
+@RequestMapping("/device/accessibility")
 @PreAuthorize("permitAll()")
-class AccessibilityController {
+class AccessibilityController(private val accessInfoService: IAccessInfoService) {
     @GetMapping
-    fun accessibility(): Boolean {
-        return true
-    }
+    fun accessibility(
+        @RequestParam("device-id") deviceId: String,
+        @RequestParam("user-id") userId: String
+    ): Boolean = accessInfoService.existAccessInfoById(AccessInfoId(deviceId, userId))
 }
