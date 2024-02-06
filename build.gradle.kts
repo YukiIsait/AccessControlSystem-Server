c9739cd2fb4b5cc3fa96e4d87674825621fb4cdd
YukiIsait <yukiisait@outlook.jp>
2024-02-06 15:37:18 +0800

    不分层JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "3.2.2"
@@ -44,4 +45,10 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<BootJar> {
+    layered {
+        enabled.set(false)
+    }
 }
