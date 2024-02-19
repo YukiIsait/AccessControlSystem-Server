d9a09b36c5bef876f71bcacb35f1d0e03725aa4f
YukiIsait <yukiisait@outlook.jp>
2024-02-19 17:40:16 +0800

    Aut

--- src/main/kotlin/tech/youko/acms/properties/JwtProperties.kt
+++ src/main/kotlin/tech/youko/acms/properties/JwtProperties.kt
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationProperties(prefix = "acms.jwt")
 data class JwtProperties(
     var secretKey: String = "49A4A069-5A4C-4756-B573-E71B1411C3D9",
-    var authoritiesKey: String = "authorities",
+    var authoritiesKey: String = "aut",
     var duration: Long = 3600000
 )
